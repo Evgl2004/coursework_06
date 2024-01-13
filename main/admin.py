1efa605a5a3474e808d29443f8f94e2c1c9e5e04
Andrey Evgl <evgl2004@yandex.ru>
2024-01-13 23:08:17 +0500

    Изменение 02. Подготовлены модели: Клиентов, Писем, Рассылки, Журнала рассылки. Заполнены админки.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Clients, Mails, SendingLists, LogSendingMails
+
+
+@admin.register(Clients)
+class ClientsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'email', 'owner')
+    list_filter = ('first_name', 'email', 'owner')
+    search_fields = ('first_name', 'email', 'owner')
+
+
+@admin.register(Mails)
+class MailsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'owner', 'subject')
+    list_filter = ('owner', )
+    search_fields = ('owner', 'subject')
+
+
+@admin.register(SendingLists)
+class SendingListsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'owner', 'data_begin', 'data_end', 'period')
+    list_filter = ('owner', )
+    search_fields = ('owner', )
+
+
+@admin.register(LogSendingMails)
+class LogSendingMailsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'client', 'data_send', 'is_done')
+    list_filter = ('client', )
+    search_fields = ('client', )
--- main/models.py
+++ main/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from datetime import datetime
+
+from django.conf import settings
 
 NULLABLE = {'null': True, 'blank': True}
+
+
+class Clients(models.Model):
+
+    first_name = models.CharField(max_lenght=100, verbose_name='имя')
+    last_name = models.CharField(max_lenght=150, verbose_name='фамилия')
+    email = models.EmailField(unique=True, verbose_name='почта')
+    comment = models.CharField(max_lenght=250, verbose_name='комментарий', **NULLABLE)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец')
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
+
+
+class Mails(models.Model):
+
+    subject = models.CharField(max_lenght=150, verbose_name='тема')
+    body = models.TextField(verbose_name='тело')
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец')
+
+    class Meta:
+        verbose_name = 'письмо'
+        verbose_name_plural = 'письма'
+
+    def __str__(self):
+        return f'{self.owner} {self.subject}'
+
+
+class SendingLists(models.Model):
+
+    PERIOD_DAY = 'day'
+    PERIOD_WEEK = 'week'
+    PERIOD_MONTH = 'month'
+
+    PERIODS = (
+        (PERIOD_DAY, 'раз в день'),
+        (PERIOD_WEEK, 'раз в неделю'),
+        (PERIOD_MONTH, 'раз в месяц')
+    )
+
+    STATUS_CREATE = 'create'
+    STATUS_START = 'start'
+    STATUS_DONE = 'done'
+
+    STATUSES = (
+        (STATUS_CREATE, 'создан'),
+        (STATUS_START, 'запущен'),
+        (STATUS_DONE, 'завершен')
+    )
+
+    data_begin = models.DateTimeField(default=datetime.now, verbose_name='дата начала')
+    data_end = models.DateTimeField(default=datetime.now, verbose_name='дата конца')
+    period = models.CharField(max_lenght=5, default='day', choices=PERIODS, verbose_name='период')
+    status = models.CharField(max_lenght=7, default='create', choices=STATUSES, verbose_name='статус')
+
+    mail = models.ForeignKey(Mails, on_delete=models.CASCADE, verbose_name='письмо')
+    clients = models.ManyToManyField(Clients, verbose_name='клиенты')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец')
+
+    class Meta:
+        verbose_name = 'список рассылки'
+        verbose_name_plural = 'списки рассылок'
+
+    def __str__(self):
+        return f'{self.owner} {self.data_begin} {self.data_end} {self.period} {self.status}'
+
+
+class LogSendingMails(models.Model):
+
+    data_send = models.DateTimeField(default=datetime.now, verbose_name='дата')
+    is_done = models.BooleanField(default=True, verbose_name='статус')
+    error_massage = models.CharField(max_lenght=150, verbose_name='ошибка', **NULLABLE)
+
+    client = models.ForeignKey(Clients, on_delete=models.CASCADE, verbose_name='клиент')
+    send_list = models.ForeignKey(SendingLists, on_delete=models.CASCADE, verbose_name='рассылка')
+
+    class Meta:
+        verbose_name = 'журнал рассылки'
+        verboseName_plural = 'журналы рассылок'
+
+    def __str__(self):
+        return f'{self.is_done} {self.client} {self.data_send}'
